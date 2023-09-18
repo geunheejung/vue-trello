a49a51359572386c547b722757e471a29d7da25e
geunheejung <geunhee0212@gmail.com>
2023-09-18 16:05:10 +0900

    login

--- src/api/index.js
+++ src/api/index.js
@@ -11,25 +11,51 @@ const instance = axios.create({
   baseURL: DOMAIN
 });
 
+instance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers = { Authorization: `Bearer ${token}` };
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
 const request = (method, url, data) => {
   return instance({
     method,
     url,
     data
-  })
-    .then(res => res.data)
-    .catch(error => {
-      const {
-        response: { status }
-      } = error;
-      debugger;
-      if (status === UNAUTHORIZED) return onUnauthorized();
-      throw Error(error);
-    });
+  }).catch(error => {
+    const {
+      response: { status, data }
+    } = error;
+
+    if (status === UNAUTHORIZED) onUnauthorized();
+    throw error;
+  });
+};
+
+export const setAuthInHeader = token => {
+  instance.defaults.headers.common["Authorization"] = token
+    ? `Bearer ${token}`
+    : null;
 };
 
 export const board = {
   fetch() {
     return request("get", "boards");
+  }
+};
+
+export const auth = {
+  login(email, password) {
+    return request("post", "login", { email, password }).then(res => {
+      return res.data;
+    });
   }
 };
