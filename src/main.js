9393a0551bdf693106b52fe85ecb3f359f292789
geunheejung <geunhee0212@gmail.com>
2023-09-18 11:53:09 +0900

    router 직접 만들기.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+const Login = { template: "<div>Login Page</div>" };
+
+const routes = {
+  "/": App,
+  "/login": Login
+};
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  el: "#app",
+  computed: {
+    VueComponent() {
+      return routes[window.location.pathname] || { template: "<div>404</div>" };
+    }
+  },
+  render(h) {
+    console.log(h);
+    return h(this.VueComponent);
+  }
+});
