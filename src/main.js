64616555b66219d29cf24d24041c2801d84a6b67
geunheejung <geunhee0212@gmail.com>
2023-09-18 12:04:35 +0900

    vue router

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
+
+Vue.use(VueRouter);
 
 const Login = { template: "<div>Login Page</div>" };
 
-const routes = {
-  "/": App,
-  "/login": Login
-};
+const NotFound = { template: "<div>Not Found</div>" };
+
+const routes = new VueRouter({
+  // hash의 경우 history API가 없을 경우 route의 대한 url 규칙을 hash 로 사용한다임.
+  mode: "history",
+  routes: [
+    { path: "/", component: App },
+    { path: "/login", component: Login },
+    { path: "*", component: NotFound }
+  ]
+});
 
 new Vue({
   el: "#app",
-  computed: {
-    VueComponent() {
-      return routes[window.location.pathname] || { template: "<div>404</div>" };
-    }
-  },
-  render(h) {
-    console.log(h);
-    return h(this.VueComponent);
-  }
+  router: routes,
+  render: h => h({ template: "<router-view></router-view>" })
 });
